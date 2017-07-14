76aec9fbcb78f58514b2a551137d468dd9a540de
Donald Rose <donaldrose@roselution.com>
2017-07-14 18:39:47 -0400

    Caught up with different branches and commit

--- src/app/experience/experience.ts
+++ src/app/experience/experience.ts
@@ -1,13 +1,35 @@
-export interface Accomplishments<T> {
-    deserialize(input: Object); T;
+import {deserialize } from 'json-typescript-mapper';
+export class Accomplishments{
+    @JsonProperty('detail')
+    accomplishments: Accomplishments;
 }
 
+
 export class Experience {
-        public companyName: string;
-        public locationCity: string;
-        public locationState: string;
-        public startDate: string;
-        public endDate: string;
-        public description: string;
-        public title: string;
+    @JsonProperty('companyName')
+    public companyName: string;
+    @JsonProperty('locationCity')
+    public locationCity: string;
+    @JsonProperty('locationState')
+    public locationState: string;
+    @JsonProperty('startDate')
+    public startDate: string;
+    @JsonProperty('endDate')
+    public endDate: string;
+    @JsonProperty('description')
+    public description: string;
+    @JsonProperty('title')
+    public title: string;
+    @JsonProperty({clazz: Accomplishments})
+    public accomplishments: Accomplishments;
+
+    constructor() {
+        this.companyName = void 0;
+        this.locationCity = void 0;
+        this.locationState = void 0;
+        this.startDate = void 0;
+        this.endDate = void 0;
+        this.description = void 0;
+        this.title = void 0;
+    }
 }
